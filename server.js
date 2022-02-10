04d75cbeda9a4b570f5949984d588fb33b04aca4
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-02-10 13:00:49 -0600

    express boilerplate

---
+++ server.js
@@ -0,0 +1,22 @@
+//imports
+const express = require('express')
+const app = express()
+require('./db')
+
+//CONFIG
+const PORT = 8000
+
+
+//MIDDLEWARE
+
+
+
+//ROUTING
+
+
+
+//SERVERSTART
+
+app.listen(PORT, () => {
+    console.log(`App started on port ${PORT}!!!!!!`)
+})
