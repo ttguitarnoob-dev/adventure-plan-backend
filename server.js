a2b5bbde28bfee39809fcf8cd8afa6f6efd872e5
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-02-10 15:23:19 -0600

    configured app to accept outside requests.  Backend is ready for mvp

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 require('./db')
+const cors = require('cors')
+
+//CONTROLLERS
 const adventuresController = require('./controllers/adventures')
 
 //CONFIG
 const PORT = 8000
-
+const acceptList = ["http://localhost:3000"]
+const options = {
+    origin: function(origin, callback){
+        console.log('origin', origin)
+        if (acceptList.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        } else {
+            callback( new Error ("Not allowed by Cors, bro"))
+        }
+    }
+}
 
 //MIDDLEWARE
+app.use(cors(options))
 app.use(express.json())
 app.use('/adventures', adventuresController)
 
